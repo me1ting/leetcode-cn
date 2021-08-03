87bab5fc6c3dd72138757c57f34faaa421473496
hybo <52079867+me1ting@users.noreply.github.com>
2021-08-03 09:20:23 +0800

    Update by using std-solution

--- src/array/shortest-unsorted-continuous-subarray/answer.go
+++ src/array/shortest-unsorted-continuous-subarray/answer.go
@@ -1,7 +1,7 @@
 package shortestunsortedcontinuoussubarray
 
 // https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
-// 可以使用贪婪算法：
+// 可以使用双指针算法：
 // 我们假设数组的左边存在一个有序子数列，数组右边存在一个有序子数列，我们称左有序子数列的右边界（包含）、右有序子数列的左边界（包含）分别为（l,r）：
 // - l==len-1，整个数组有序，返回0
 // - l==0&&r==len-1，整个输入无序，返回len
@@ -16,11 +16,14 @@ package shortestunsortedcontinuoussubarray
 //
 // 如：1,5,6,7,8,9,2,3,4，nums[l]=9，nums[new_l]=5
 // 如：1,2,3,7,4,5,6,8，nums[r]=4，nums[new_r]=6
+//
+//
 
+/*
 func findUnsortedSubarray(nums []int) int {
 	n := len(nums)
 	l, r := 0, n-1
-	for l < r && nums[l] < nums[l+1] {
+	for l < r && nums[l] <= nums[l+1] {
 		l++
 	}
 
@@ -28,7 +31,7 @@ func findUnsortedSubarray(nums []int) int {
 		return 0
 	}
 
-	for l < r && nums[r-1] < nums[r] {
+	for l < r && nums[r-1] <= nums[r] {
 		r--
 	}
 
@@ -57,4 +60,39 @@ func findUnsortedSubarray(nums []int) int {
 	r--
 
 	return r - l + 1
+}
+*/
+
+// 题解：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/solution/zui-duan-wu-xu-lian-xu-zi-shu-zu-by-leet-yhlf/
+// 方法1排序后逐一两端比较，时间和空间效率都很低
+//
+// 方法2看似一次遍历，实则是两次遍历，只是放在了一个循环里面；代码很精炼，但硬塞了二次遍历导致难以理解。
+// 其基本思想是：
+// - 从左往右遍历，记录最大值，从而找到（最短无序连续子数组）右边界。如果更新了最大值，不影响右边界，如果nums[i]<max，说明i属于最短无序连续子数组，可以暂时更新右边界为i。
+// - 从右网左遍历，记录最小值，从而找到左边界。如果更新最小值，不影响左边界；如果nums[i]>min，说明i属于最短无序连续子数组，暂时更新左边界为i。
+// 由于缓存的问题，运行时间效率略低于双指针算法，但时间复杂度没有区别。
+
+func findUnsortedSubarray(nums []int) int {
+	n := len(nums)
+	max, right := nums[0], -1
+	for i := 0; i < n; i++ {
+		if nums[i] < max {
+			right = i
+		} else {
+			max = nums[i]
+		}
+	}
+	if right == -1 {
+		return 0
+	}
+
+	min, left := nums[n-1], n
+	for j := n - 1; j >= 0; j-- {
+		if nums[j] > min {
+			left = j
+		} else {
+			min = nums[j]
+		}
+	}
+	return right - left + 1
 }
